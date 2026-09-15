45c4e2b33fa616e5b22115f0555ada03e8eccca9
pphongsakorn-cloud <p.phongsakorn@gmail.com>
2026-09-15 05:39:04 +0000

    Cache internal PWA launcher
    Refresh the service-worker cache and include the internal launcher assets.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'the-rise-food-v2';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './logo.png', './icon-192.png', './icon-512.png', './hero-room.jpg'];
+const CACHE_NAME = 'the-rise-food-v3';
+const APP_SHELL = ['./', './index.html', './internal.html', './manifest.webmanifest', './internal-manifest.webmanifest', './logo.png', './icon-192.png', './icon-512.png', './hero-room.jpg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
